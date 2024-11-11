2d746b75a442e0bea7c3a1688b25032771312d10
Your Name <your-email@example.com>
2024-11-11 17:04:27 +0530

    Logging support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use dotenv::dotenv;
+use tokio::net::TcpListener;
+use tower::ServiceBuilder;
+use tower_http::trace::TraceLayer;
+
+async fn index() -> &'static str {
+    "Server is Running"
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    dotenv()?;
+
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
+
+    let service_builder = ServiceBuilder::new().layer(TraceLayer::new_for_http());
+
+    let listener = TcpListener::bind("localhost:3000").await?;
+    let router = Router::new().route("/", get(index)).layer(service_builder);
+    axum::serve(listener, router).await?;
+    Ok(())
 }
