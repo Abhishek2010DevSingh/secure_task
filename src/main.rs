963716e75d704de983462f56068c7ba8102c9579
Your Name <your-email@example.com>
2024-11-12 21:36:05 +0530

    simple stetup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 use axum::{routing::get, Router};
 use dotenv::dotenv;
 use tokio::net::TcpListener;
@@ -16,9 +18,13 @@ async fn main() -> anyhow::Result<()> {
         .with_max_level(tracing::Level::DEBUG)
         .init();
 
+    let addr = format!("{}:{}", env::var("HOST")?, env::var("PORT")?);
+
+    tracing::info!("Running on http://{addr}");
+
     let service_builder = ServiceBuilder::new().layer(TraceLayer::new_for_http());
 
-    let listener = TcpListener::bind("localhost:3000").await?;
+    let listener = TcpListener::bind("{addr}").await?;
     let router = Router::new().route("/", get(index)).layer(service_builder);
     axum::serve(listener, router).await?;
     Ok(())
